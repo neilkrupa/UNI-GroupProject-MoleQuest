c137f737c12f67da681398b97dd846db429d6b65
Astonex <softukitu@gmail.com>
2015-01-15 01:59:16 +0000

    Fix tabs and spaces

--- MoleQuest/Game.cc
+++ MoleQuest/Game.cc
@@ -1,7 +1,7 @@
-//	Game.cpp
-//	This file is part of MoleQuest
+//  Game.cpp
+//  This file is part of MoleQuest
 //
-//	For purpose, see Game.h
+//  For purpose, see Game.h
 
 #include "stdafx.h"
 #include "Game.h"
@@ -10,21 +10,21 @@ void Game::Start()
 {
   // If the game state is kUninitialised then return as the game has already
   // been set up before
-	if (game_state_ != kUninitialised)
-	  return;
+  if (game_state_ != kUninitialised)
+    return;
 
   // Create the main window
-	main_window_.create(sf::VideoMode(1024, 768, 32), "Mole Quest");
+  main_window_.create(sf::VideoMode(1024, 768, 32), "Mole Quest");
 
   // Set the game state to kPlaying
-	game_state_ = Game::kPlaying;
+  game_state_ = Game::kPlaying;
 
   // Continously call the game loop
-	while (!IsExiting())
-		GameLoop();
+  while (!IsExiting())
+    GameLoop();
 
   // Call Exit() to clean up all resources and exit the program
-	Exit();
+  Exit();
 }
 
 void Game::Exit()
@@ -34,33 +34,32 @@ void Game::Exit()
 
 bool Game::IsExiting()
 {
-	return game_state_ == Game::kExiting;
+  return game_state_ == Game::kExiting;
 }
 
 void Game::GameLoop()
 {
-	sf::Event event;
+  sf::Event event;
 
   // Get the last event. The while loop will make it wait for an event to occur
-	while (main_window_.pollEvent(event)) {
+  while (main_window_.pollEvent(event)) {
     // Handle the event based upon what state the game is currently in
-		switch (game_state_) {
-      case kPlaying: {
-        main_window_.clear(sf::Color(255, 0, 0));
-        main_window_.display();
+    switch (game_state_) {
+     case kPlaying: {
+       main_window_.clear(sf::Color(255, 0, 0));
+       main_window_.display();
 
-        if (event.type == sf::Event::Closed)
-          game_state_ = Game::kExiting;
+       if (event.type == sf::Event::Closed)
+         game_state_ = Game::kExiting;
 
         break;
       }
-      case kShowingSplash: {}
+     case kShowingSplash: {}
+     
+     case kPaused: {}
 
-      case kPaused: {}
-
-      case kShowingMenu: {}
-
-		}
+     case kShowingMenu: {}
+    }
 	}
 }
 
--- MoleQuest/Game.h
+++ MoleQuest/Game.h
@@ -13,30 +13,30 @@ public:
   // finally calls the game loop continously whilst the game state isn't
   // set to kExiting. The game state MUST be set to kUninitialised 
   // otherwise this function will just return without doing anything.
-	static void Start();
+  static void Start();
 
 private:
   // Will clean up any resources, kill the window and exit the program
-	static void Exit();
+  static void Exit();
 
   // Returns true if the game state is set to kExiting
-	static bool IsExiting();
+  static bool IsExiting();
 
   // The main loop of the game. Will continously poll events and then
   // proceed to deal with the events based on the current game state.
   // The loop will also handle the timing of each loop and getting
   // user input handled.
-	static void GameLoop();
+  static void GameLoop();
 
   // An enumeration to hold all possible states the game can be in
-	enum GameState { kUninitialised, kShowingSplash, kPaused, kShowingMenu, 
+  enum GameState { kUninitialised, kShowingSplash, kPaused, kShowingMenu, 
                     kPlaying, kExiting };
 
   // A static reference to hold the current state of the game
-	static GameState game_state_;
+  static GameState game_state_;
 
   // A static reference to the main game window
-	static sf::RenderWindow main_window_;
+  static sf::RenderWindow main_window_;
 };
 
 #endif
