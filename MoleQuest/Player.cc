9b10a03c4eb45c40ba06059557697b27042e7167
Kieran Affleck <Kieran.Affleck@gmail.com>
2015-01-22 11:53:41 +0000

    Player.cc edited online with Bitbucket

--- MoleQuest/Player.cc
+++ MoleQuest/Player.cc
@@ -4,3 +4,25 @@
 Player::Player() {}
 
 Player::~Player() {}
+
+void Player::move(unsigned int w,unsigned int h){
+	float y = GameObject::GetPosition().y;
+	float x = GameObject::GetPosition().x;
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+		if (y - .5 >= 0)
+			GameObject::SetPosition(x, y - .5);
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+		if (x - .5 >= 0)
+			GameObject::SetPosition(x - .5, y);
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+		if (x + .5 < w)
+			GameObject::SetPosition(x + .5, y);
+		
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+		if (y + .5 < h)
+			GameObject::SetPosition(x, y + .5);
+
+}
