3297e468f71710cc0ad1601aaf2c26ce06ddac18
Kieran Affleck <Kieran.Affleck@gmail.com>
2015-02-08 15:28:23 +0000

    Changes relating to the shop

--- MoleQuest/Player.h
+++ MoleQuest/Player.h
@@ -17,6 +17,11 @@ class Player : public GameObject {
   void MoveLeft();
   void MoveRight();
   void Jump();
+  void Buy(std::string purchase);
+  void Upgrade(std::string upgradable);
+
+  int getHealthLevel();
+  int getSpeedLevel();
 
   float GetVelocityX();
   float GetVelocityY();
@@ -24,9 +29,28 @@ class Player : public GameObject {
  private:
   AnimationHandler* animation_handler_;
 
+  struct Stat{
+    public:
+		std::string name;
+		float value;
+		int cost;
+		int level;
+		int max;
+		//This is the amount the cost will increase per level
+		int increase;
+		//This is the amount each level will increase the value
+		int amount;
+  }; 
+
+  int coins;
+
   float velocity_x_;
   float velocity_y_;
   bool is_moving_;
+  
+  std::list<Weapon> weapons_;
+  std::list<Stat> stats_;
+  
 };
 
 #endif
