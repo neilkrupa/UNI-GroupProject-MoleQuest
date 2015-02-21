15beb640813eabff7124a9c974b582ef6a0f7784
Matt Jones <matt.jazzer@gmail.com>
2015-02-21 12:45:40 +0000

    Header for mole.cc

---
+++ MoleQuest/Mole.h
@@ -0,0 +1,22 @@
+#ifndef MOLE_H_
+#define MOLE_H_
+
+#include "GameObject.h"
+#include "AnimationHandler.h"
+#include "Player.h"
+class Mole : public GameObject {
+public:
+	Mole(Player& Player_);
+	~Mole();
+	void Update(float lag);
+	void Draw(float interp, sf::RenderWindow& window);
+	//void Damage(int value);
+	Player *player;
+	int coins;
+	float velocity_x_;
+	float velocity_y_;
+	bool is_moving_;
+private:
+	AnimationHandler* animation_handler_;
+};
+#endif
