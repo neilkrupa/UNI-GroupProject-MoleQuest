bd59fb9d82e52e390d454c7cf59548b663980029
Matt Jones <matt.jazzer@gmail.com>
2015-02-23 18:06:33 +0000

    FastMole.h created online with Bitbucket

---
+++ MoleQuest/FastMole.h
@@ -0,0 +1,12 @@
+#ifndef FASTMOLE_H_
+#define FASTMOLE_H_
+#include "Player.h"
+#include "Mole.h"
+class FastMole :public Mole{
+public:
+	FastMole(Player* p);
+	~FastMole();
+private:
+
+};
+#endif
