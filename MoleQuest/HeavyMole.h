16854ed1446999f3f6fd2abfcad8de753c25099a
Matt Jones <matt.jazzer@gmail.com>
2015-02-23 17:54:51 +0000

    HeavyMole.h created online with Bitbucket

---
+++ MoleQuest/HeavyMole.h
@@ -0,0 +1,12 @@
+#ifndef HEAVYMOLE_H_
+#define HEAVYMOLE_H_
+#include "Player.h"
+#include "Mole.h"
+class HeavyMole :public Mole{
+public:
+	HeavyMole(Player* p);
+	~HeavyMole();
+private:
+
+};
+#endif
