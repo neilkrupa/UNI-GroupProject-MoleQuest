837cb2049d1b6265c2628320a9352074f47e64eb
Matt Jones <matt.jazzer@gmail.com>
2015-02-24 21:38:59 +0000

    FastMole.cc edited online with Bitbucket

--- MoleQuest/FastMole.cc
+++ MoleQuest/FastMole.cc
@@ -4,12 +4,13 @@
 FastMole::FastMole(Player* p) :Mole(p){
 	mole_.velocity_x_ = 0;
 	mole_.velocity_y_ = 0;
-	mole_.max_velocity_ = 0.3;
+	mole_.max_velocity_ = 0.25;
 	mole_.max_value = 3;
 	mole_.curr_value = mole_.max_value;
-	mole_.coins = 5;
+	mole_.coins = rand() % 30;
 	mole_.type = 2;
 	mole_.damage = 30;
-	SetImage();
+	mole_.position_ = rand() % 1024;
+	Set();
 }
 FastMole::~FastMole() {}
