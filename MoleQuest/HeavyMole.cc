692caad570a992ed185e856f99eb70bb735c4b47
Matt Jones <matt.jazzer@gmail.com>
2015-02-24 21:23:02 +0000

    HeavyMole.cc edited online with Bitbucket

--- MoleQuest/HeavyMole.cc
+++ MoleQuest/HeavyMole.cc
@@ -2,14 +2,18 @@
 #include "HeavyMole.h"
 
 HeavyMole::HeavyMole(Player* p):Mole(p){
+	srand(time(NULL));
 	mole_.velocity_x_ = 0;
 	mole_.velocity_y_ = 0;
 	mole_.max_velocity_ = 0.03;
 	mole_.max_value = 200;
 	mole_.curr_value = mole_.max_value;
-	mole_.coins = 5;
+	mole_.coins = rand() %30;
 	mole_.type = 1;
 	mole_.damage = 1;
-	SetImage();
+	mole_.position_ = rand() % 1024;
+	printf("position = %d\n", mole_.position_);
+	printf("coins = %d\n", mole_.coins);
+	Set();
 }
 HeavyMole::~HeavyMole() {}
